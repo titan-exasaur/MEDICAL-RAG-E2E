14ccfc6332e1d0ec4f5f7ecf855cc8fcaf91fc50
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-11 13:01:18 +0530

    project structure created

---
+++ template.sh
@@ -0,0 +1,19 @@
+# creating directories
+mkdir -p src
+mkdir -p research
+
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+
+touch research/trials.ipynb
+
+# status message
+echo "Project template generation successful..."

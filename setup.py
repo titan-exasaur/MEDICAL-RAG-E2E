544b675dbb64d3e4bd8ada3e3fca9b50e8d8f702
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-11 13:12:37 +0530

    project setup completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version='0.1.0',
+    author='tiny_tachyon',
+    author_email='tinytachyon14341@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
